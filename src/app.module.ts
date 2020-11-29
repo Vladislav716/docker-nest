990f5990a65fe8ce362a131cd80e16f917822f66
Vladislav716 <vladislav.ivanilov2019@gmail.com>
2020-11-29 07:23:19 -0800

    initialize

---
+++ src/app.module.ts
@@ -0,0 +1,41 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+
+const objMySQLConf = {
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: parseInt(process.env.MYSQL_PORT) || 3306,
+  database: process.env.MYSQL_DATABASE || 'test',
+  user: process.env.MYSQL_USER || 'dev',
+  password: process.env.MYSQL_PASSWORD || 'Liverpool1@&',
+  synchronize: true,
+  entities: ['dist/**/*.entity{.ts,.js}']
+};
+
+// const objPostgreSQLConf = {
+//   host: process.env.POSTGRESQL_HOST || 'localhost',
+//   port: parseInt(process.env.POSTGRESQL_PORT) || 5432,
+//   database: process.env.POSTGRESQL_DATABASE || 'test',
+//   user: process.env.POSTGRESQL_USER || 'dev',
+//   password: process.env.POSTGRESQL_PASSWORD || 'Liverpool1@&',
+//   synchronize: true,
+//   entities: ['dist/**/*.entity{.ts,.js}']
+// };
+
+console.log(objMySQLConf);
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      ...objMySQLConf
+    }),
+    UsersModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
